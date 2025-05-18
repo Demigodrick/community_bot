fa6f4b43e8d25a6f3f26c6140a19697ede70ef7b
gazzby <gazby@thegreatgazby.com>
2025-05-18 19:18:17 +1000

    Update healthcheck.py
    Read the log file in reverse by chunks, and search only 200 lines back

--- healthcheck.py
+++ healthcheck.py
@@ -1,24 +1,53 @@
 import datetime
 import sys
+import os
+import itertools
+
+# Ref: https://stackoverflow.com/questions/2301789/how-to-read-a-file-in-reverse-order
+def reverse_readline(filename, buf_size=8192):
+    """A generator that returns the lines of a file in reverse order"""
+    with open(filename, 'rb') as fh:
+        segment = None
+        offset = 0
+        fh.seek(0, os.SEEK_END)
+        file_size = remaining_size = fh.tell()
+        while remaining_size > 0:
+            offset = min(file_size, offset + buf_size)
+            fh.seek(file_size - offset)
+            buffer = fh.read(min(remaining_size, buf_size))
+            # remove file's last "\n" if it exists, only for the first buffer
+            if remaining_size == file_size and buffer[-1] == ord('\n'):
+                buffer = buffer[:-1]
+            remaining_size -= buf_size
+            lines = buffer.split('\n'.encode())
+            # append last chunk's segment to this chunk's last line
+            if segment is not None:
+                lines[-1] += segment
+            segment = lines[0]
+            lines = lines[1:]
+            # yield lines in this chunk except the segment
+            for line in reversed(lines):
+                # only decode on a parsed line, to avoid utf-8 decode error
+                yield line.decode()
+        # Don't yield None if the file was empty
+        if segment is not None:
+            yield segment.decode()
 
 LOG_FILE_PATH = 'resources/zippy.log'
 
 
 def check_last_log_timestamp(log_file):
-    now = datetime.datetime.utcnow()
+    now = datetime.datetime.now(datetime.UTC)
     time_format = "%Y-%m-%d %H:%M:%S,%f"
 
     try:
-        with open(log_file, 'r') as file:
-            lines = file.readlines()
-            if not lines:
+        for line in itertools.islice(reverse_readline(log_file), 250):
+            if not line:
                 print("Log file is empty.")
                 return False
 
-            last_line = lines[-1]
-            timestamp_str = last_line.split(' - ')[0].strip()
-            last_log_time = datetime.datetime.strptime(
-                timestamp_str, time_format)
+            timestamp_str = line.split(' - ')[0].strip()
+            last_log_time = datetime.datetime.strptime(timestamp_str, time_format).astimezone(tz=datetime.UTC)
 
             # print(f"Last log time: {last_log_time}")
             # print(f"Current time: {now}")
